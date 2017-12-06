d7aa2b78987674535cd3cc33dd8620fe94880e5b
brccl <34248371+brccl@users.noreply.github.com>
2017-12-06 09:01:25 +0100

    Initial commit

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+
+    public void ToGame()
+    {
+        SceneManager.LoadScene("Lv1");
+
+
+    }
+
+    public void ToOptions()
+    {
+
+        SceneManager.LoadScene("Option Menu");
+
+
+
+    }
+
+    public void ToMainMenu()
+    {
+
+        SceneManager.LoadScene("Main Menu");
+
+
+
+    }
+
+    public void Quiterino()
+    {
+        Application.Quit();
+
+    }
+
+
+
+
+}
