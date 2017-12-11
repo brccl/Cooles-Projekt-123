e00b29aba8e719b832330ed7d3f616cac9eb091a
brccl <34248371+brccl@users.noreply.github.com>
2017-12-11 16:49:38 +0100

    jklhjkl
    .öä#klöä#

---
+++ Assets/scripts/Level1End.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level1End : MonoBehaviour {
+
+    [SerializeField] private string NewLevel;
+    [SerializeField] public bool Done = false;
+
+ 
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Hub");
+            Done = true;
+
+            
+            
+
+        }
+    }
+
+}
