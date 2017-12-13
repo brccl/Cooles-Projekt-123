fed95e1f7aa8a68c9ab6ef8a2759e6bf9b54ce25
brccl <34248371+brccl@users.noreply.github.com>
2017-12-13 19:12:44 +0100

    13.12
    swag

---
+++ Assets/scripts/Coins.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour {
+
+    [SerializeField] public int Wert = 1;
+    [SerializeField] public int CoinZ = 0;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+
+
+            CoinManager.AddCoins(Wert);
+        }
+
+    }
+
+
+    private void Start()
+        {
+
+
+
+        }
+}
+
+ 
