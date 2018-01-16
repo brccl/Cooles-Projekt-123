09508ab64eae4a32a2ec404eec8736452d8b2e45
brccl <34248371+brccl@users.noreply.github.com>
2018-01-16 16:06:39 +0100

    fdgsdg

---
+++ Assets/scripts/Albert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Albert : MonoBehaviour
+{
+
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            SceneManager.LoadScene("Albert");
+
+
+
+
+
+
+        }
+    }
+
+
+
+}
